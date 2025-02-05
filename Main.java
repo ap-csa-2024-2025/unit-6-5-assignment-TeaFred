0bb8af0f95340021698a171aaf669713deb0e79f
user <email>
2025-02-05 19:06:35 +0000

    asdasdasd

--- Main.java
+++ Main.java
@@ -4,22 +4,52 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Create some arrays here to test your methods
+    String[] theStringArr = {"hill", "mold", "tree", "lake"};
+    reverse(theStringArr);
     
   }
 
   public static void reverse(String[] arr)
   {
-
+    String reverseWord = new String();
+    for (int i = 0; i < arr.length; i++)
+    {
+      reverseWord = "";
+      for (int j = arr[i].length(); j > 0; j--)
+      {
+        reverseWord += arr[i].substring(j-1,j);
+      }
+      arr[i] = reverseWord;
+      System.out.println(arr[i]);
+    }
   }
 
   public int product(int[] arr)
   {
-    return 0;
+    int theTotal = 0;
+    for (int i = 0; i < arr.length; i++)
+    {
+      if (i = 0)
+      {
+        theTotal += arr[i];
+      }
+      else
+      {
+        theTotal = theTotal * arr[i];
+      }
+    }
+    return theTotal;
   }
 
   public double average(double[] arr)
   {
-    return 0.0;
+    double theTotal = 0;
+    double theAvg = 0;
+    for (elem:arr)
+    {
+      theTotal += elem;
+    }
+    theAvg = theTotal/arr.length;
+    return theAvg;
   }
 }
